267f957f63268e13abb3c44ec6f06b799349f022
snehal <snehal.kale@eternussolutions.com>
2020-09-30 10:41:52 +0530

    assignment2

--- app/code/MageMastery/FirstLayout/Controller/Page/View.php
+++ app/code/MageMastery/FirstLayout/Controller/Page/View.php
@@ -1,18 +1,26 @@
 <?php
 
-//declare(strict_types=1);
+declare(strict_types=1);
 
 namespace MageMastery\FirstLayout\Controller\Page;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Result\Page;
 
 class View extends Action
 {
     public function execute()
     {
-        /** @var Page $resultPage */
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        /** @var Page $page */
+        $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        /** @var Template $block */
+        $block = $page->getLayout()->getBlock('magemastery.first.layout.example');
+        $block->setData('greeting_param', 'Hello World !!');
+        $block->setData('image_param', 'images/magento-logo.svg');
+
+        return $page;
     }
 }
