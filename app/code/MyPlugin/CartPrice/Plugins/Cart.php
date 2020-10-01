ae50afb0cc477300d44279813de5856df059bb66
hi-meral <miralmaradia@yahoo.com>
2020-10-01 20:05:18 +0530

    Plugin Development | Adding extra product cost to cart

--- app/code/MyPlugin/CartPrice/Plugins/Cart.php
+++ app/code/MyPlugin/CartPrice/Plugins/Cart.php
@@ -1,15 +1,11 @@
 <?php
-/*
- * Name : Cart Price
- * Use : Add addition amount to actual product price when being added into cart
- * Developer : Miral Maradia
- * Create Date : 30 Sep, 2020
-*/
+
 namespace MyPlugin\CartPrice\Plugins;
 
 class Cart
 {
     protected $logger;
+
     public function __construct(\Psr\Log\LoggerInterface $logger)
     {
         $this->logger = $logger;
