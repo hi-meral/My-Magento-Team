2f31544961b9b5aafa85e320c2366bf48ab95ca1
hi-meral <miralmaradia@yahoo.com>
2020-10-01 19:58:53 +0530

    Page with Template

--- app/code/MyPlugin/CartPrice/Plugins/Cart.php
+++ app/code/MyPlugin/CartPrice/Plugins/Cart.php
@@ -9,8 +9,14 @@ namespace MyPlugin\CartPrice\Plugins;
 
 class Cart
 {
+    protected $logger;
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     public function aroundAddProduct(\Magento\Checkout\Model\Cart $subject, \Closure $proceed, $productInfo, $requestInfo=null)
     {
+        //$this->logger->debug("Test Log 1 oct");
         $additionalCost = 50;
         $productInfo['price'] = $productInfo['price'] + $additionalCost;
         $result = $proceed($productInfo, $requestInfo);
