f8b4c3ce70b648308a3dda5ff19af5bda81fb825
snehal <snehal.kale@eternussolutions.com>
2020-10-04 13:04:14 +0530

    block code

--- app/code/User/Module/Block/Index.php
+++ app/code/User/Module/Block/Index.php
@@ -6,17 +6,8 @@ use User\Module\Model\ResourceModel\PostUser\Collection;
 
 class Index extends Template
 {
-    /**
-     * @var Collection
-     */
     private $collection;
 
-    /**
-     * Hello constructor.
-     * @param Template\Context $context
-     * @param Collection $collection
-     * @param array $data
-     */
     public function __construct(Template\Context $context, Collection $collection, array $data = [])
     {
         parent::__construct($context, $data);
@@ -27,3 +18,5 @@ class Index extends Template
         return $this->getUrl('userdataModule/page/postuserdata');
     }
 }
+
+
