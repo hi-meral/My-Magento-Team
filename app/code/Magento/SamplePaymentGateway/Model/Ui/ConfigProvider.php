68d503da672868ab72f6998131fe44036736c563
hi-meral <miralmaradia@yahoo.com>
2020-10-08 13:52:07 +0530

    Custom Payment Gateway

---
+++ app/code/Magento/SamplePaymentGateway/Model/Ui/ConfigProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Magento\SamplePaymentGateway\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\SamplePaymentGateway\Gateway\Http\Client\ClientMock;
+
+/**
+ * Class ConfigProvider
+ */
+final class ConfigProvider implements ConfigProviderInterface
+{
+    const CODE = 'sample_gateway';
+
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [
+            'payment' => [
+                self::CODE => [
+                    'transactionResults' => [
+                        ClientMock::SUCCESS => __('Success'),
+                        ClientMock::FAILURE => __('Fraud')
+                    ]
+                ]
+            ]
+        ];
+    }
+}
